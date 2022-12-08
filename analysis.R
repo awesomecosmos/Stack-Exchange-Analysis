13e57a6d964831a05e503c91e6b3f35e98824850
Aayushi Verma <56360465+awesomecosmos@users.noreply.github.com>
2022-12-07 21:07:19 -0500

    Started data basic data analysis

--- analysis.R
+++ analysis.R
@@ -1,3 +1,11 @@
 library(readr)
+library(dplyr)
+library(tidyr)
+
 df <- read_csv("data/astronomy.csv")
 head(df)
+
+df2 <- df %>% 
+  select(c(CreationDate,Score,AnswerCount,CommentCount,Tags)) %>% 
+  distinct() %>% 
+  filter(!is.na(Tags))
